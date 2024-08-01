6d0d29e651a34b68abda4a76547656dbc897b538
goreshitcore <165133434+goreshitcore@users.noreply.github.com>
2024-08-01 14:49:17 +1200

    Create CircleAreaCalculator.java

---
+++ CircleAreaCalculator.java
@@ -0,0 +1,8 @@
+
+public class CircleAreaCalculator {
+	public static void main(String[] args) {
+		double radius = 10.5;
+		double area = Math.PI * radius * radius;
+		System.out.println("area = "+area);
+	}
+}
