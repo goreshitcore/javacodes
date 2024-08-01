7a8652a9eda804989f61ea6a038a38b78ca51402
goreshitcore <165133434+goreshitcore@users.noreply.github.com>
2024-08-01 14:48:29 +1200

    Create Convert.java

---
+++ Convert.java
@@ -0,0 +1,14 @@
+public class Convert {
+    public static void main(String args[]) 
+    {
+        char four = '5'- 1;
+		char letter = 'C' - 2;
+		System.out.println("four = "+four);
+		System.out.println("letter = "+letter);
+		int fourValue = four;
+		int letterValue = letter;
+
+		System.out.println("fourValue = "+fourValue);
+		System.out.println("letterValue = "+letterValue);
+    }
+}
