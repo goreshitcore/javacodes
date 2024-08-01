e527501fffe9ea0f8f9b771758bc821618a3c108
goreshitcore <165133434+goreshitcore@users.noreply.github.com>
2024-08-01 14:49:01 +1200

    Create Relations.java

---
+++ Relations.java
@@ -0,0 +1,16 @@
+public class Relations {
+	public static void main(String[] args) {
+		int x = 2;
+		int z = 8;
+		System.out.println("x =" +x);
+		System.out.println("z =" +z);
+		System.out.println("" + (x == z));
+		System.out.println("" + (x != z));
+		System.out.println("" +(x > z));
+		System.out.println("" + (x < z));
+		System.out.println("" + (x >= z));
+		
+	
+		
+	}
+}
