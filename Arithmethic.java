1209774b710aa1fcb1422714c9dbdbdeebb45b8a
goreshitcore <165133434+goreshitcore@users.noreply.github.com>
2024-08-01 14:41:07 +1200

    Create Arithmethic.java

---
+++ Arithmethic.java
@@ -0,0 +1,16 @@
+
+public class Arithmethic {
+	public static void main(String[] args) {
+		int x = 5;
+		int y = 3;
+		int sum = x + y;
+		int product = x * y;
+		int mod = x % y;
+		System.out.println("x = "+x);
+		System.out.println("y = "+y);
+		System.out.println("sum = "+sum);
+		System.out.println("product = "+product);
+		System.out.println("mod = "+mod);
+		
+	}
+}
