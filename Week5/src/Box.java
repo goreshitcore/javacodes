78763abc296fab41f0f36d7aa1e64c312f9ff080
goreshitcore <165133434+goreshitcore@users.noreply.github.com>
2024-08-27 12:48:51 +1200

    Update Box.java

--- Week5/src/Box.java
+++ Week5/src/Box.java
@@ -2,13 +2,13 @@ public class Box {
     private int height;
     private int width;
     private int depth;
-//constructor
+
     public Box(int height, int width, int depth) {
         this.height = height;
         this.width = width;
         this.depth = depth;
     }
-//get 
+
     public int getHeight() {
         return height;
     }
@@ -23,31 +23,24 @@ public class Box {
 
     @Override
     public String toString() {
-        return "Box{" +
-                "height=" + height +
-                ", width=" + width +
-                ", depth=" + depth +
-                '}';
+        return "height = " + height + "\nwidth = " + width + "\ndepth = " + depth;
     }
 
     public boolean fits(Box box) {
-        return this.height > box.height && this.width > box.width && this.depth > box.depth;
+        return height > box.height && width > box.width && depth > box.depth;
     }
 
     public boolean fits(Box box1, Box box2) {
-        return this.fits(box1) && box1.fits(box2);
+        return fits(box1) && box1.fits(box2);
     }
 
     public static void main(String[] args) {
         Box box1 = new Box(11, 21, 31);
         System.out.println(box1.toString());
-
         Box box2 = new Box(21, 31, 41);
         System.out.println(box2.toString());
-
         Box box3 = new Box(31, 41, 51);
         System.out.println(box3.toString());
-
-        System.out.println("Does box1 fit box2 and box3? " + box1.fits(box2, box3));
+        System.out.println("fits = " + box1.fits(box2, box3));
     }
 }
