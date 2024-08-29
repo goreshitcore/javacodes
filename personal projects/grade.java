c72c83b05ea32e6fd1fbe885589fe65dab060ca6
goreshitcore <165133434+goreshitcore@users.noreply.github.com>
2024-08-29 13:44:18 +1200

    Create grade.java

---
+++ personal projects/grade.java
@@ -0,0 +1,17 @@
+
+public class Grade {
+	public int percentage;
+	
+	public Grade(int percentage)
+	{
+	this.setPercentage(percentage);
+	}
+	public void setPercentage(int percentage)
+	{
+	this.percentage = percentage;
+	}
+	public int getPercentage()
+	{
+	return this.percentage;
+	}
+}
